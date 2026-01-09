b3cb1ea205eb950294b292ef3fb8e87f9588fc81
stereokontroll <126024989+stereokontroll@users.noreply.github.com>
2026-01-09 17:35:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,12 +7,14 @@ const ASSETS = [
   "./icon-512.png"
 ];
 
+// Installeren en bestanden opslaan (cachen)
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+// Ophalen uit cache als er geen internet is
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
