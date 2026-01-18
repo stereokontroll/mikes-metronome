fcdb92b489eb08354de4f7da8cc5da46fedc9d07
stereokontroll <126024989+stereokontroll@users.noreply.github.com>
2026-01-18 20:16:36 +0100

    v3.5 Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "metronome-v3.01"; // Verhoog dit nummer bij elke update!
+const CACHE_NAME = "metronome-v3.5"; // Verhoog dit nummer bij elke update!
 const ASSETS = [
   "./",
   "./index.html",
